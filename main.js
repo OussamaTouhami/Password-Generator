7e746527a6ba3c17cc990cffe32a23f8b30edd6d
Oussama Touhami <touhami200622@gmail.com>
2025-04-27 13:03:25 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+let passLength = document.getElementById("passLength");
+let lengthVal = document.getElementById("lengthVal");
+
+passLength.addEventListener("input",() =>{
+    lengthVal.textContent = passLength.value;
+})
+
+
+function generate(){
+    let lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+    let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let numbers = "1234567890";
+    let characters = "&(-_)=+°}]@^`|[{#";
+
+    let lc = document.getElementById("lc");
+    let uc = document.getElementById("uc");
+    let nums = document.getElementById("nums");
+    let chars = document.getElementById("chars");
+    let generatedPassword = document.getElementById("generatedPass");
+
+
+    let allowedChars = "";
+    let password = "";
+
+    if (lc.checked){
+        allowedChars += lowerLetters;
+    }
+    if (uc.checked){
+        allowedChars += upperLetters;
+    }
+    if (nums.checked){
+        allowedChars += numbers;
+    }
+    if (chars.checked){
+        allowedChars += characters;
+    }
+
+    for (let i = 0;i < passLength.value;i++){
+        let randomItem = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomItem];
+    }
+
+    generatedPassword.value = password;
+
+
+
+}
+let copyBtn = document.getElementById("copyBtn");
+let copyAlert = document.getElementById("copyAlert");
+copyAlert.style.display = "none";
+
+copyBtn.addEventListener("click",function(){
+    let password = document.getElementById("generatedPass");
+    password.select();
+    navigator.clipboard.writeText(password.value);
+    copyBtn.style.background = "#80b918"
+    copyBtn.textContent = "Copied !"
+    copyAlert.style.display = "block";
+    setTimeout(() => {
+        copyBtn.style.background = "orange"
+        copyBtn.textContent = "Copy"
+    copyAlert.style.display = "none";
+        
+    },4000);
+})
